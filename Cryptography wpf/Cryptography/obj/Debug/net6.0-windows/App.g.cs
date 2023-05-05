9bbfa08fc6a2727ec2f8438ddbca17a59f4cd027
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-05-05 23:37:55 +0430

    create new folder for source

--- Cryptography wpf/Cryptography/obj/Debug/net6.0-windows/App.g.cs
+++ Cryptography wpf/Cryptography/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace Cryptography {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Cryptography {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public static void Main() {
             Cryptography.App app = new Cryptography.App();
             app.InitializeComponent();
